e034c15513a659f5e2e3e648b7392954e55f7dc6
zhaoxiaodong <zhaoxiaodong@didachuxing.com>
2022-07-20 15:13:31 +0800

    行为模式

---
+++ gof/behavior/obserser.go
@@ -0,0 +1,56 @@
+package behavior
+
+import "fmt"
+
+// Author      : zhaoxiaodong
+// Create      : 2022/7/20 10:28
+// Description : 观察者模式
+
+//观察者模式用于触发联动。
+//
+//一个对象的改变会触发其它观察者的相关动作，而此对象无需关心连动对象的具体实现。
+//当对象间存在一对多关系时，则使用观察者模式（Observer Pattern）。比如，当一个对象被修改时，则会自动通知依赖它的对象
+
+type Subject struct {
+	observers []Observer
+	context   string
+}
+
+func NewSubject() *Subject {
+	return &Subject{
+		observers: make([]Observer, 0),
+	}
+}
+
+func (s *Subject) Attach(o Observer) {
+	s.observers = append(s.observers, o)
+}
+
+func (s *Subject) notify() {
+	for _, o := range s.observers {
+		o.Update(s)
+	}
+}
+
+func (s *Subject) UpdateContext(context string) {
+	s.context = context
+	s.notify()
+}
+
+type Observer interface {
+	Update(*Subject)
+}
+
+type Reader struct {
+	name string
+}
+
+func NewReader(name string) *Reader {
+	return &Reader{
+		name: name,
+	}
+}
+
+func (r *Reader) Update(s *Subject) {
+	fmt.Printf("%s receive %s\n", r.name, s.context)
+}
